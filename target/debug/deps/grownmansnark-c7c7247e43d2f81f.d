c33c667f993b32d8e2d82580e4e741840971948a
Logan Norman <logannormanss@gmail.com>
2023-04-15 17:08:32 -0700

    init rust

---
+++ target/debug/deps/grownmansnark-c7c7247e43d2f81f.d
@@ -0,0 +1,5 @@
+/Users/logno/Documents/work/github/grownmansnark/target/debug/deps/grownmansnark-c7c7247e43d2f81f: src/main.rs
+
+/Users/logno/Documents/work/github/grownmansnark/target/debug/deps/grownmansnark-c7c7247e43d2f81f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/grownmansnark.d
@@ -0,0 +1,1 @@
+/Users/logno/Documents/work/github/grownmansnark/target/debug/grownmansnark: /Users/logno/Documents/work/github/grownmansnark/src/main.rs
